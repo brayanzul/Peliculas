d96a0eb607f7f7fc2573635230bd288a44cd03ca
Brayan Zuluaga <brayanzuluaga93@hotmail.com>
2022-12-20 17:58:44 -0500

    Add the Movie project: Today I worked on Screens and widgets

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:peliculas/widgets/widgets.dart';
+
+class HomeScreen extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('Peliculas de Cines'),
+        elevation: 0,
+        actions: [
+          IconButton(
+            icon: const Icon( Icons.search_outlined ),
+            onPressed: () {},
+          )
+        ],
+      ),
+      body: Column(
+        children: [
+
+          CardSwiper()
+
+          //Listas horizontales de Peliculas 
+
+        ],
+      )
+    );
+  }
+}

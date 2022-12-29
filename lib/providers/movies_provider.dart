1b511bc6d11bbeed7717a331c6600b19361a98ce
Brayan Zuluaga <brayanzuluaga93@hotmail.com>
2022-12-29 12:01:20 -0500

    Add the Movie project: Today I worked on Debouncer, Movies Provider, Search Delegate

--- lib/providers/movies_provider.dart
+++ lib/providers/movies_provider.dart
@@ -1,7 +1,9 @@
-import 'dart:convert';
+import 'dart:async';
+//import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:peliculas/helpers/debouncer.dart';
 import 'package:peliculas/models/models.dart';
 
 class MoviesProvider extends ChangeNotifier{
@@ -16,6 +18,13 @@ class MoviesProvider extends ChangeNotifier{
   Map<int, List<Cast>> moviesCast = {};
 
   int _popularPage = 0;
+
+  final debouncer = Debouncer(
+    duration: Duration(milliseconds: 500),
+  );
+
+  final StreamController<List<Movie>> _suggestionStreamController = new StreamController.broadcast();
+  Stream<List<Movie>> get suggestionStream => this._suggestionStreamController.stream;
 
   MoviesProvider() {
     print('MoviesProvider inicializando');
@@ -91,6 +100,25 @@ class MoviesProvider extends ChangeNotifier{
     final searchResponse = SearchResponse.fromjson( response.body );
 
     return searchResponse.results;
+
+  }
+
+  void getSuggestionsByQuery( String searchTerm ){
+
+    debouncer.value = '';
+    debouncer.onValue =(value) async {
+      
+      //print('Tenemos valor a buscar: $value');
+      final results = await this.searchMovies(value);
+      this._suggestionStreamController.add( results );
+
+    };
+
+    final timer = Timer.periodic(Duration(milliseconds: 300), ( _ ){
+      debouncer.value = searchTerm;
+    });
+
+    Future.delayed(Duration( milliseconds: 301 )).then(( _ ) => timer.cancel());
 
   }
 
